c023a143677f939eb72fbc9842f10fe2a94d5e0a
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-02-01 13:09:05 -0600

    python

---
+++ python_folder/python/fundamentals/basic1.py
@@ -0,0 +1,31 @@
+print("hello")
+#for x in range (0,151,1):
+    #print(x)
+
+#for x in range (5,1001,5):
+#    print(x)
+
+#Ask if it is rigth
+#for x in range (1,101,1):
+#    if x%5 == 0:
+#        print("Coding")
+#    if x%10 == 0:
+#        print("Coding Dojo")
+#    print(x)  
+
+#sum=0
+#for x in range (1,500000,2):
+#    sum += x
+#    print(sum)
+
+#for x in range (2018,0,-4):
+#    print(x)
+
+#Ask how I should do in a array?
+lowNum=2
+highNum=9
+mult=3
+for x in range(2,10):
+    if x%3==0:
+        print(x)
+
