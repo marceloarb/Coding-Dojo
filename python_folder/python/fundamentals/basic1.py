67449c0c8a6f54ce60c9b19a98d6dc6e5ef350d1
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-02-01 22:49:24 -0600

    update algo

--- python_folder/python/fundamentals/basic1.py
+++ python_folder/python/fundamentals/basic1.py
@@ -22,10 +22,98 @@ print("hello")
 #    print(x)
 
 #Ask how I should do in a array?
-lowNum=2
-highNum=9
-mult=3
-for x in range(2,10):
-    if x%3==0:
-        print(x)
+#lowNum=2
+#highNum=9
+#mult=3
+#for x in range(2,10):
+#    if x%3==0:
+#        print(x)
+
+#9
+#8
+#10
+#1
+#def a():
+#    return 5
+#print(a())
+#2
+#def a():
+#    return 5
+#print(a()+a())
+#3
+#def a():
+#    return 5
+#    return 10
+#print(a())
+
+#4
+#def a():
+#    return 5
+#    print(10)
+#print(a())
+
+
+#5
+#def a():
+#    print(5)
+#x = a()
+#print(x)
+
+
+#6
+#def a(b,c):
+#    print(b+c)
+#print(a(1,2) + a(2,3))
+
+
+#7
+#def a(b,c):
+#    return str(b)+str(c)
+#print(a(2,5))
+
+
+
+    #10
+#11
+#b = 500
+#print(b)
+#def a():
+#    b = 300
+#    print(b)
+#print(b)
+#a()
+#print(b)
+
+#12
+#13
+#b = 500
+#print(b)
+#def a():
+#    b = 300
+#    print(b)
+#    return b
+#print(b)
+#b=a()
+#print(b)
+
+
+
+
+
+
+#14
+
+
+
+#15
+def a():
+    print(1)
+    x = b()
+    print(x)
+    return 10
+def b():
+    print(3)
+    return 5
+y = a()
+print(y)
 
