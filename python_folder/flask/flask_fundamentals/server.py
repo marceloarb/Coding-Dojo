c4514d5b5709d731693a271d3d8334f7df2608ab
marceloarb <59586505+marceloarb@users.noreply.github.com>
2020-02-05 12:57:40 -0600

    flask

---
+++ python_folder/flask/flask_fundamentals/server.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template #We need this two lines at the beginin
+app = Flask(__name__)
+
+@app.route('/dojo')
+def marcelo():
+    return "I made my first request!"
+
+@app.route('/say/<int:number>/<other>')
+def show(number,other):
+    return str(number)
+
+
+@app.route('/a')
+def server():
+    return render_template('a.html',phrase="Here is my phrase",name=["marcelo","brandom","joao"])
+    
+
+
+#we need to have this two line at the end
+if __name__ == "__main__":
+    app.run(debug=True)
